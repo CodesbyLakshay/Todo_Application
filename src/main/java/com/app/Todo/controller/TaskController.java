7011e8b982b62d0e12d4a9801722dc34fb222037
lakshayjain <lakshay2007jain@gmail.com>
2025-03-17 21:32:31 +0530

    Final Commit

---
+++ src/main/java/com/app/Todo/controller/TaskController.java
@@ -0,0 +1,43 @@
+package com.app.Todo.controller;
+
+
+import com.app.Todo.model.Task;
+import com.app.Todo.services.TaskServices;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+//@RequestMapping("/tasks")
+public class TaskController {
+    private final TaskServices taskServices;
+
+    public TaskController(TaskServices taskServices) {
+        this.taskServices = taskServices;
+    }
+
+    @GetMapping
+    public String getTasks(Model model){
+        List<Task> tasks = taskServices.getAllTasks();
+        model.addAttribute("tasks",tasks);
+        return "tasks";
+    }
+
+    @PostMapping
+    public String createTask(@RequestParam String title){
+        taskServices.createTask(title);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/delete")
+    public String deleteTask(@PathVariable Long id){
+        taskServices.deleteTask(id);
+        return "redirect:/";
+    }
+    @GetMapping("/{id}/toggle")
+    public String toggleTask(@PathVariable Long id){
+        taskServices.toggleTask(id);
+        return "redirect:/";
+    }
+}
